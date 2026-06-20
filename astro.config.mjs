d98bb3c2a5d43292e924517dde4f243639269265
Rhgx <eren.serbetcioglu@gmail.com>
2026-06-20 14:00:43 +0300

    feat(seo): add sitemap and social metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
+  site: "https://vanillafortress.github.io",
+  integrations: [sitemap()],
   vite: {
     plugins: [
       yaml()
